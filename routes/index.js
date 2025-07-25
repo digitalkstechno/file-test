e1911ea39871a9c321c2eb6f866c4260a27072d6
digitalkstechno <digitalkstechno@gmail.com>
2025-07-25 16:06:42 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+const multer = require("multer");
+
+const fs = require("fs");
+const path = require("path");
+
+const publicUploadPath = path.join(__dirname, "../public/uploads");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    fs.mkdir(publicUploadPath, { recursive: true }, (err) => {
+      cb(err, publicUploadPath);
+    });
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/", upload.any(), function (req, res, next) {
+  try {
+    let file = req.files;
+    let fileDetails = file.map((el) => {
+      return `http://localhost:3000/uploads/${el.filename}`;
+    });
+    res.status(201).json({
+      status: "Success",
+      message: fileDetails,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "Fail",
+      message: error,
+    });
+  }
+});
+
+module.exports = router;
